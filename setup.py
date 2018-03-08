94b2537948366bf052de70a6ce41f3928109d892
William Pelrine <rudy@ionic.io>
2018-03-08 15:45:35 -0600

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='taskpool',
+      version='0.0.1',
+      description='Task runner for Python microservices',
+      url='https://github.com/ionic-team/task-pool',
+      author='Ionic',
+      author_email='hi@ionic.io',
+      install_requires=['redis'])
